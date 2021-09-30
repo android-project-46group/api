9a032df8bef319dfd4a2e370c4e212bd772ec042
kokoichi206 <tt.300607@gmail.com>
2021-10-01 02:09:12 +0900

    chore: other groups

--- firebase/save_db.py
+++ firebase/save_db.py
@@ -12,10 +12,9 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-GROUP_NAME = "nogizaka"
 
-# Json のデータを、DB に保存する！
-DATA_PATH = f'../scraping/{GROUP_NAME}/detailed_infos.txt'
+
+
 
 # データの保存
 # データ例
@@ -26,7 +25,7 @@ DATA_PATH = f'../scraping/{GROUP_NAME}/detailed_infos.txt'
 #     "星座": "しし座",
 #     "身長": "154cm",
 # },
-def save_info():
+def save_info(group_name, DATA_PATH):
 
     print(DATA_PATH)
     import json
@@ -38,34 +37,37 @@ def save_info():
         print(name_en)
         print(member_info["生年月日"])
 
-        doc_ref = db.collection(f'{GROUP_NAME}').document(f'{name_en}')
+        doc_ref = db.collection(f'{group_name}').document(f'{name_en}')
         doc_ref.set({
             u'name_en': name_en,
             u'name_ja': member_info["名前"],
             u'birthday': member_info["生年月日"],
-            u'height': member_info["身長"]
+            u'height': member_info["身長"],
+            u'blood_type': member_info["血液型"]
         })
 
-def save_urls():
+def save_urls(group_name):
     import json
     # member_infos = ""
-    with open(f"url_infos_{GROUP_NAME}.txt", mode='rt', encoding='utf-8') as f:
+    with open(f"url_infos_{group_name}.txt", mode='rt', encoding='utf-8') as f:
         url_infos = json.load(f)
 
     for name_en, url in url_infos.items():
         print(name_en)
         print(url)
 
-        doc_ref = db.collection(f'{GROUP_NAME}').document(f'{name_en}')
+        doc_ref = db.collection(f'{group_name}').document(f'{name_en}')
         doc_ref.set({
             u'img_url': url
         })
 
-def main():
+def main(group_name):
+    # Json のデータを、DB に保存する！
+    DATA_PATH = f'../scraping/{group_name}/detailed_infos.txt'
 
     import json
     # member_infos = ""
-    with open(f"url_infos_{GROUP_NAME}.txt", mode='rt', encoding='utf-8') as f:
+    with open(f"url_infos_{group_name}.txt", mode='rt', encoding='utf-8') as f:
         url_infos = json.load(f)
 
     print(DATA_PATH)
@@ -78,15 +80,18 @@ def main():
         print(name_en)
         print(member_info["生年月日"])
 
-        doc_ref = db.collection(f'{GROUP_NAME}').document(f'{name_en}')
+        doc_ref = db.collection(f'{group_name}').document(f'{name_en}')
         doc_ref.set({
             u'name_en': name_en,
             u'name_ja': member_info["名前"],
             u'birthday': member_info["生年月日"],
             u'height': member_info["身長"],
-            u'img_url': url_infos[name_en]
+            u'blood_type': member_info["血液型"],
+            u'generation': member_info["世代"],
+            u'img_url': url_infos[name_en],
         })
 
 if __name__ == "__main__":
-    main()
+    # GROUP_NAME = "nogizaka"   hinatazaka      sakurazaka
+    main("nogizaka")
     # save_urls()
--- firebase/save_pictures.py
+++ firebase/save_pictures.py
@@ -1,5 +1,10 @@
+from pyasn1_modules.rfc2459 import UniversalString
 import pyrebase
 
+"""
+save pictures to Firebare Storage and
+make url_infos_{groupName}.txt to save to Database
+"""
 
 # // Your web app's Firebase configuration
 # // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,7 +20,9 @@ config = {
 firebase_storage = pyrebase.initialize_app(config)
 storage = firebase_storage.storage()
 
-GROUP_NAME = "nogizaka"
+# GROUP_NAME = "nogizaka"
+# GROUP_NAME = "hinatazaka"
+GROUP_NAME = "sakurazaka"
 
 def uploadPic(path, name):
     # storage.child("saka").child(GROUP_NAME).child(path).put(f"{name}.jpeg")
@@ -47,7 +54,12 @@ if __name__ == "__main__":
     DICT_END    = '}'
     with open(f'url_infos_{GROUP_NAME}.txt', mode='w') as f:
         f.write(f'{DICT_START}{NEW_LINE}')
+        cnt = 0
         for name_en, url in url_infos.items():
-            f.write(f'{TAB}"{name_en}": {url},{NEW_LINE}')
+            cnt += 1
+            if cnt == len(url_infos):
+                f.write(f'{TAB}"{name_en}": "{url}"{NEW_LINE}')
+            else:
+                f.write(f'{TAB}"{name_en}": "{url}",{NEW_LINE}')
         f.write(f'{DICT_END}{NEW_LINE}')
 
