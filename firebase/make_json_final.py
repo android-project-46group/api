8adcf390137611d9cea18e7aa36db447bb7de470
kokoichi206 <tt.300607@gmail.com>
2021-10-15 00:16:25 +0900

    chore: make json for db (with golang)

---
+++ firebase/make_json_final.py
@@ -0,0 +1,89 @@
+
+
+# データの保存
+# データ例
+# "akimotomanatsu": {
+#     "名前": "秋元 真夏",
+#     "生年月日": "1993年8月20日",
+#     "血液型": "B型",
+#     "星座": "しし座",
+#     "身長": "154cm",
+# },
+def save_info(group_name, DATA_PATH):
+
+    print(DATA_PATH)
+    import json
+    # member_infos = ""
+    with open(DATA_PATH, mode='rt', encoding='utf-8') as f:
+        member_infos = json.load(f)
+
+    for name_en, member_info in member_infos.items():
+        print(name_en)
+        print(member_info["生年月日"])
+
+        doc_ref = db.collection(f'{group_name}').document(f'{name_en}')
+        doc_ref.set({
+            u'name_en': name_en,
+            u'name_ja': member_info["名前"],
+            u'birthday': member_info["生年月日"],
+            u'height': member_info["身長"],
+            u'blood_type': member_info["血液型"]
+        })
+
+def save_urls(group_name):
+    import json
+    # member_infos = ""
+    with open(f"url_infos_{group_name}.txt", mode='rt', encoding='utf-8') as f:
+        url_infos = json.load(f)
+
+    for name_en, url in url_infos.items():
+        print(name_en)
+        print(url)
+
+        doc_ref = db.collection(f'{group_name}').document(f'{name_en}')
+        doc_ref.set({
+            u'img_url': url
+        })
+
+def main(group_name):
+    # Json のデータを、DB に保存する！
+    DATA_PATH = f'../scraping/{group_name}/detailed_infos.txt'
+
+    import json
+    # member_infos = ""
+    with open(f"url_infos_{group_name}.txt", mode='rt', encoding='utf-8') as f:
+        url_infos = json.load(f)
+
+    with open(f"blogUrls_{group_name}.txt", mode='rt', encoding='utf-8') as f:
+        blog_url_infos = json.load(f)
+
+    print(DATA_PATH)
+    import json
+    # member_infos = ""
+    with open(DATA_PATH, mode='rt', encoding='utf-8') as f:
+        member_infos = json.load(f)
+
+    member_infos_final = {}
+    for name_en, member_info in member_infos.items():
+        print(name_en)
+        print(member_info["生年月日"])
+
+        member_infos_final[name_en] = {            
+            'name_ja': member_info["名前"],
+            'birthday': member_info["生年月日"],
+            'height': member_info["身長"],
+            'blood_type': member_info["血液型"],
+            'generation': member_info["世代"],
+            'blog_url': blog_url_infos[name_en],
+            'img_url': url_infos[name_en],
+        }
+
+    import json
+    with open(f'data/{group_name}.json', 'w') as f:
+        json.dump(member_infos_final, f, indent=2)
+
+
+if __name__ == "__main__":
+    GROUP_NAMES = ["nogizaka", "hinatazaka", "sakurazaka"]
+    for group in GROUP_NAMES:
+        main(group)
