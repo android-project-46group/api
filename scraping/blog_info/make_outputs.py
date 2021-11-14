118d52565449b08d4195914b9f124dd19ae7e0fb
kokoichi206 <tt.300607@gmail.com>
2021-11-14 16:10:11 +0900

    fix: datatime style (#8)

--- scraping/blog_info/make_outputs.py
+++ scraping/blog_info/make_outputs.py
@@ -27,11 +27,27 @@ def main(group_name):
             'name': name,
             'blog_url': blog_url,
             'last_blog_img': blog_info['pic_url'],
-            'last_updated_at': blog_info['last_updated_at'],
+            'last_updated_at': zeroFillIfNecessary(group_name, blog_info['last_updated_at']),
         })
 
     with open(f'outputs/{group_name}.json', 'w') as f:
         json.dump(tmp, f, indent=2)
+
+def zeroFillIfNecessary(group_name, dateTime):
+    res = dateTime
+    # input 2021.6.2 21:06 -> output 2021.06.02 21:06
+    if group_name == "hinatazaka":
+        separator1 = '.'
+        separator2 = ' '
+        separated = dateTime.split(separator2)
+        time = separated[1]
+        separatedDate = separated[0].split(separator1)
+        separatedDate[1] = ('0' + separatedDate[1])[-2:]
+        separatedDate[2] = ('0' + separatedDate[2])[-2:]
+
+        date = separator1.join(separatedDate)
+        res = date + separator2 + time
+    return res
 
 
 if __name__ == "__main__":
